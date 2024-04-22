4c56e0b12fe561219a7cfbb8dd3965db76d0d8ef
RodrigoDGoulart <rodinizgoulart@gmail.com>
2024-04-22 07:48:34 -0300

    feat: pacientes get list endpoint

--- src/main/java/med/voll/api/controller/PacienteController.java
+++ src/main/java/med/voll/api/controller/PacienteController.java
@@ -1,7 +1,10 @@
 package med.voll.api.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 import jakarta.validation.Valid;
 import med.voll.api.entities.Paciente;
 import med.voll.api.records.DadosCadastroPaciente;
+import med.voll.api.records.DadosListagemPaciente;
 import med.voll.api.repositories.PacienteRepository;
 
 @RestController
@@ -23,5 +27,11 @@ public class PacienteController {
   @Transactional
   public void cadastrar(@RequestBody @Valid DadosCadastroPaciente dados) {
     repository.save(new Paciente(dados));
+  }
+
+  @GetMapping
+  @Transactional
+  public Page<DadosListagemPaciente> listar(Pageable paginacao) {
+    return repository.findAll(paginacao).map(DadosListagemPaciente::new);
   }
 }
