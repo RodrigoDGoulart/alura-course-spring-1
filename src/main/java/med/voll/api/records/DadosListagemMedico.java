677694042d075c88a2b18d96081cc5342b039566
RodrigoDGoulart <rodinizgoulart@gmail.com>
2024-04-22 09:48:25 -0300

    feat: include id field on medicos list endpoint

--- src/main/java/med/voll/api/records/DadosListagemMedico.java
+++ src/main/java/med/voll/api/records/DadosListagemMedico.java
@@ -3,6 +3,7 @@ package med.voll.api.records;
 import med.voll.api.entities.Medico;
 
 public record DadosListagemMedico(
+  Long id,
   String nome,
   String email,
   String crm,
@@ -10,7 +11,7 @@ public record DadosListagemMedico(
 ) {
 
   public DadosListagemMedico(Medico medico) {
-    this(medico.getNome(), medico.getEmail(), medico.getCrm(), medico.getEspecialidade());
+    this(medico.getId(), medico.getNome(), medico.getEmail(), medico.getCrm(), medico.getEspecialidade());
   }
 
 }
